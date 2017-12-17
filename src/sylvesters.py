9972cb8d2edbe301d9386badf27a7b1fdaa5f299
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2017-12-17 23:19:01 +0000

    package Sylvester's resultant

---
+++ src/sylvesters.py
@@ -0,0 +1,45 @@
+"""
+A function for creating the Sylvester's matrix and calculating the Sylvester's
+resultant. Literature: http://comet.lehman.cuny.edu/vpan/pdf/DEKPalg.pdf
+"""
+
+import sympy as sym
+
+
+def sylvester_matrix(p, q, x):
+    """
+    A function that takes two non zero polynomials, of degree m and n respectively
+    and returns the Sylvester's matrix $(m+n)\times(m+n)$.
+
+    Parameters
+    ----------
+    p: sympy expression
+        A non zero polynomial of degree $m$.
+    q: sympy expression
+        A non zero polynomial of degree $n$.
+    x: sympy symbol
+        Variable for which we are solving.
+    """
+    p_polynomial = sym.Poly(p, x)
+    q_polynomial = sym.Poly(q, x)
+    
+    m_degree, n_degree = p_polynomial.degree(), q_polynomial.degree()
+    
+    p_coefficients = p_polynomial.all_coeffs()
+    q_coefficients = q_polynomial.all_coeffs()
+    
+    matrix_size = m_degree + n_degree
+    
+    matrix = []
+    for row in [p_coefficients, q_coefficients]:
+        matrix.append(row)
+        while len(row) < matrix_size:
+            row = [0] + row
+            matrix.append(row)
+    
+    for row in matrix:
+        while len(row) < matrix_size:
+            row.append(0)
+
+    return sym.Matrix(matrix)
+
---
+++ tests/test_sylvesters.py
@@ -0,0 +1,43 @@
+"""
+A file to test Sylvester's matrix.
+"""
+import sys
+sys.path.insert(0, '../src/')
+
+import unittest
+import sympy as sym
+
+from sylvesters import sylvester_matrix
+
+
+class TestSylvester(unittest.TestCase):
+
+    def test_sylvester_matrix_generic_case(self):
+        """Test Sylvester's matrix for a generic case."""
+        a = sym.IndexedBase("a")
+        b = sym.IndexedBase("b")
+        x = sym.symbols("x")
+
+        p = a[1] * x + a[0]
+        q = b[2] * x ** 2 + b[1] * x + b[0]
+
+        matrix = sylvester_matrix(p, q, x)
+        self.assertEqual(matrix, sym.Matrix([[a[1], a[0], 0],
+                                             [0, a[1], a[0]],
+                                             [b[2], b[1], b[0]]]))
+        self.assertEqual(matrix.det(),
+                       a[0] ** 2 * b[2] - a[0] * a[1] * b[1] + a[1] ** 2 * b[0])
+    
+    def test_sylvester_matrix_example(self):
+        """Test Sylvester's matrix for a numerical example."""
+        x = sym.symbols("x")
+
+        p = x ** 2 - 5 * x + 6
+        q = x ** 2 - 3 * x + 2
+
+        matrix = sylvester_matrix(p, q, x)
+        self.assertEqual(matrix, sym.Matrix([[1, -5, 6, 0],
+                                             [0, 1, -5, 6],
+                                             [1, -3, 2, 0],
+                                             [0, 1, -3, 2]]))
+        self.assertEqual(matrix.det(), 0)
