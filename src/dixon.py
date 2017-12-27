4d41787ec69ec9aca06f2c73e023eff4e537b27c
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2017-12-27 01:13:16 +0000

    package Dixon's resultant

--- src/dixon.py
+++ src/dixon.py
@@ -1,9 +1,15 @@
 """
 A class for creating the Dixon's matrix and calculating the Dixon's
-resultant. Literature:
+resultant. Literature: https://dl.acm.org/citation.cfm?doid=190347.190372.
+
+Dixon's resultant is a generalisation of the Cayley-Bezout resultant.
 """
 
 import sympy as sym
+import numpy as np
+import functools
+
+from sympy.polys.monomials import itermonomials
 
 class DixonResultant():
     """
@@ -12,79 +18,115 @@ class DixonResultant():
 
     def __init__(self, polynomials, variables):
         """
-        A class that takes two lists, a list of polynomials and list of 
+        A class that takes two lists, a list of polynomials and list of
         variables. Returns the Dixon matrix of the multivariate system.
 
         Parameters
-        ----------          
-        n: integer
-            Number of variables
-        m: integer
-            Number of $n$-degree polynomials $(n + 1)$.
+        ----------
         variables: list
-            A list of all $n$ variables
+            A list of all n variables
         polynomials : list of sympy polynomials
-            A  list of $m$ $n$-degree polynomials
-        degree: integer
+            A  list of m n-degree polynomials
         """
-
         self.polynomials = polynomials
         self.variables = variables
 
         self.n = len(self.variables)
         self.m = len(self.polynomials)
 
-        a = sym.IndexedBase("alpha")
-        self.dummy_variables = [a[i] for i in range(self.n)]
-        
-        self.max_degree = self.degree()
+        self.dummy_variables = self.get_dummy_variables()
+        self.max_degrees = self.get_max_degrees()
 
-    def degree(self):
-        degree = []
+    def get_dummy_variables(self):
+        """
+        Returns
+        -------
+
+        dummy_variables: list
+            A list of n alpha variables. These are the replacing variables
+        """
+        a = sym.IndexedBase("alpha")
+        dummy_variables = [a[i] for i in range(self.n)]
+
+        return dummy_variables
+
+    def get_max_degrees(self):
+        """
+        Returns
+        -------
+
+        degrees: list
+            A list of the d_max of each variable. The max degree is the
+            max(degree(p_1, x_i), ..., degree(p_m, x_i))
+        """
+        max_degrees = []
         for v in self.variables:
-            degree.append(max([sym.Poly(f(*self.variables)).degree(v) for f in self.polynomials]))
-        return sum(degree)
+            max_degrees.append(max([sym.Poly(f(*self.variables)).degree(v) for f in self.polynomials]))
+        return max_degrees
 
     def get_dixon_polynomial(self):
-        
+        """
+        Returns
+        -------
+
+        dixon_polynomial: sympy polynomial
+            A sympy polynomial which formulated by Dixon's formulation.
+            Dixon's polynomial is calculated as:
+
+            delta = Delta(A) / ((x_1 - a_1) ... (x_n - a_n)) where,
+
+            A =  |p_1(x_1,... x_n), ..., p_n(x_1,... x_n)|
+                 |p_1(a_1,... x_n), ..., p_n(a_1,... x_n)|
+                 |...             , ...,              ...|
+                 |p_1(a_1,... a_n), ..., p_n(a_1,... a_n)|
+        """
         if self.m != (self.n + 1):
             raise Exception('Method invalid for given combination.')
-        
+
         # first row
         rows = [[poly(*self.variables) for poly in self.polynomials]]
-        
+
         temp = [*self.variables]
         iterator = iter(self.dummy_variables)
-        
+
         for idx in (idx for idx in range(self.n)):
             temp[idx] = next(iterator)
             rows.append([poly(*temp) for poly in self.polynomials])
- 
-        matrix = sym.Matrix(rows)
-        product_of_differences = functools.reduce(lambda x, y: x * y, 
+
+        A = sym.Matrix(rows)
+        product_of_differences = functools.reduce(lambda x, y: x * y,
                                                   [a - b for a, b in zip(self.variables, self.dummy_variables)])
-        return sym.Poly((matrix.det() / product_of_differences).factor(), *self.dummy_variables)
+        dixon_polynomial = (A.det() / product_of_differences).factor()
+        return sym.Poly(dixon_polynomial, *self.dummy_variables)
 
-    
-    def get_coeff_of_alpha(self, polynomial):
+    def get_coefficients_of_alpha(self, polynomial):
         """
-        Returns the coefficients of terms x_i...x_n in \delta when viewed as a polynomial
-        of \alpha_i...\alpha_n.
+        Returns
+        --------
+        coefficients: list
+            A list of coefficients (in x_i, ..., x_n terms) of the power products
+            a_1, ..., _n in Dixon's polynomial
         """
-        coeffs = []
-        for monoms_powers in polynomial.monoms():
-            mono = functools.reduce(lambda i, j: i * j,
-                                    [a ** b for a, b in zip(self.dummy_variables, monoms_powers)])
-            coeffs.append(polynomial.coeff_monomial(mono))
+        coefficients = []
+        for powers in polynomial.monoms():
+            monomial = functools.reduce(lambda i, j: i * j,
+                                    [a ** b for a, b in zip(self.dummy_variables, powers)])
+            coefficients.append(polynomial.coeff_monomial(monomial))
 
-        return coeffs
+        return coefficients
     
-    def dixon_matrix(self, coeffs, size):
+    def get_dixon_matrix(self, polynomial):
         """
         Construct the Dixon matrix from the coefficients of polynomial \alpha. Each coefficient is
         viewed as a polynomial of x and y.
-        """  
-        mononomials = list(itermonomials(variables, self.max_degree))
-        array = np.array([[sym.Poly(c, *variables).coeff_monomial(m) for m in mononomials] for c in coeffs])
-    
-        return sym.Matrix(np.delete(array, np.nonzero((array==0).sum(axis=0) == size), axis=1))
+        """
+        coefficients = self.get_coefficients_of_alpha(polynomial)
+        size = len(polynomial.monoms())
+        monomials = list(itermonomials(self.variables, sum(self.max_degrees)))
+
+        array = np.array([[sym.Poly(c, *self.variables).coeff_monomial(m) for m in monomials]
+                                                         for c in coefficients])
+
+        dixon_matrix = sym.Matrix(np.delete(array, np.nonzero((array==0).sum(axis=0) == size), 
+                                                                        axis=1))
+        return dixon_matrix
--- tests/test_dixon.py
+++ tests/test_dixon.py
@@ -9,24 +9,88 @@ import sympy as sym
 
 from dixon import DixonResultant
 
+c, d = sym.symbols("a, b")
+x, y = sym.symbols("x, y")
+
+p = sym.lambdify((x, y), c * x + y)
+q = sym.lambdify((x, y), x + d * y)
+polynomials = [p, q]
+variables = [x, y]
+
+dixon = DixonResultant(polynomials=polynomials, variables=variables)
+
 class TestDixonResultant(unittest.TestCase):
 
     def test_dixon_resultant_init(self):
         """Test init method of DixonResultant."""
-        c, d = sym.symbols("a, b")
-        x, y = sym.symbols("x, y")
-
-        p = sym.lambdify((x, y), c * x + y)
-        q = sym.lambdify((x, y), x + d * y)
-
-        polynomials = [p, q]
-        variables = [x, y]
-
-        dixon = DixonResultant(polynomials=polynomials, variables=variables)
+        a = sym.IndexedBase("alpha")
 
         self.assertEqual(dixon.polynomials, polynomials)
         self.assertEqual(dixon.variables, variables)
         self.assertEqual(dixon.n, 2)
         self.assertEqual(dixon.m, 2)
-        self.assertEqual(len(dixon.dummy_variables), 2)
-        self.assertEqual(dixon.max_degree, 2)
+        self.assertEqual(dixon.dummy_variables, [a[0], a[1]])
+        self.assertEqual(dixon.max_degrees, [1, 1])
+
+    def test_get_dummy_variables(self):
+
+        dummy_variables = dixon.get_dummy_variables()
+
+        self.assertIsInstance(dummy_variables, list)
+        self.assertEqual(len(dummy_variables), 2)
+
+    def test_get_max_degrees(self):
+
+        max_degrees = dixon.get_max_degrees()
+
+        self.assertIsInstance(max_degrees, list)
+        self.assertEqual(len(max_degrees), 2)
+
+    def test_get_dixon_polynomial_numerical(self):
+        """Test Dixon's polynomial for a numerical example."""
+
+        x, y = sym.symbols('x, y')
+        a = sym.IndexedBase("alpha")
+
+        p = sym.lambdify((x, y), x + y)
+        q = sym.lambdify((x, y), x ** 2 + y **3)
+        h = sym.lambdify((x, y), x ** 2 + y)
+
+        dixon = DixonResultant([p, q, h], [x, y])
+        polynomial = -x * y ** 2 * a[0] - x * y ** 2 * a[1] - x * y * a[0] * a[1] \
+        - x * y * a[1] ** 2 - x * a[0] * a[1] ** 2 + x * a[0] - y ** 2 * a[0] * a[1] \
+        + y ** 2 * a[1] - y * a[0] * a[1] ** 2 + y * a[1] ** 2
+
+        self.assertEqual(dixon.get_dixon_polynomial().factor(), polynomial)
+
+    def test_get_coefficients_of_alpha_numerical(self):
+        """Test Dixon's coefficients of a_1,...,a_n products for a numerical example."""
+
+        x, y = sym.symbols('x, y')
+        a = sym.IndexedBase("alpha")
+
+        p = sym.lambdify((x, y), x + y)
+        q = sym.lambdify((x, y), x ** 2 + y **3)
+        h = sym.lambdify((x, y), x ** 2 + y)
+
+        dixon = DixonResultant([p, q, h], [x, y])
+        coefficients = [-x - y, -x * y - y ** 2, -x * y ** 2 + x, -x  * y + y,
+                        -x * y ** 2 + y ** 2]
+        polynomial = dixon.get_dixon_polynomial()
+
+        self.assertEqual(dixon.get_coefficients_of_alpha(polynomial), coefficients)
+
+    def test_get_dixon_matrix(self):
+        """Test Dixon's coefficients of a_1,...,a_n products for a numerical example."""
+
+        x, y = sym.symbols('x, y')
+        a = sym.IndexedBase("alpha")
+
+        p = sym.lambdify((x, y), x + y)
+        q = sym.lambdify((x, y), x ** 2 + y **3)
+        h = sym.lambdify((x, y), x ** 2 + y)
+
+        dixon = DixonResultant([p, q, h], [x, y])
+        polynomial = dixon.get_dixon_polynomial()
+
+        self.assertEqual(dixon.get_dixon_matrix(polynomial).det(), 0)
